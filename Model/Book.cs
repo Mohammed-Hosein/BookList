ab72c0385fc73545b2178a2091728157014453c6
mohammed hosein <mohammed-norge@hotmail.com>
2020-12-08 14:56:05 +0100

    Add project files.

---
+++ Model/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList.Model
+{
+    public class Book
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public String Name { get; set; }
+        public String Author { get; set; }
+        public String ISBN { get; set; }
+    }
+}
